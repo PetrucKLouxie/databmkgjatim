4bd84b823d2552bd338e296e44a47c64d4c6d5a6
PetrucKLouxie <49406010+PetrucKLouxie@users.noreply.github.com>
2026-03-01 10:16:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -515,8 +515,8 @@ with st.sidebar:
             # =========================
             # PREVIEW DATA
             # =========================
-                    st.write("Kolom sebelum process:", df.columns.tolist())
                     df_check = process_data(df_check)
+                    st.write("Kolom sebelum process:", df.columns.tolist())
                     st.write(df_check.dtypes)
                     st.markdown("### 👁️ Preview Data")
                     st.dataframe(df_check.head(20), use_container_width=True)
@@ -729,6 +729,7 @@ Semakin tinggi skor, semakin besar potensi variabilitas atau kejadian cuaca sign
 
 else:
     st.warning("⚠️ Masukkan file excel ke folder 'data/' sesuai nama stasiun.")
+
 
 
 
